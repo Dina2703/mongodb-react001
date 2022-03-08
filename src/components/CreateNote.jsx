8751639d6218a70038b4a5357bd6f7e89652546c
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-08 18:13:52 -0500

    create note form

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -4,6 +4,28 @@ function CreateNote() {
   return (
     <div className="container">
       <h3>Create a Note</h3>
+      <form>
+        <div className="form-group mb-3">
+          <input
+            name="title"
+            autoComplete="off"
+            type="text"
+            className="form-control"
+            placeholder="note title"
+          />
+        </div>
+        <div>
+          <textarea
+            placeholder="note content"
+            className="form-control mb-3"
+            autoComplete="off"
+          />
+        </div>
+
+        <button type="button" name="content" className="btn btn-success">
+          add note
+        </button>
+      </form>
     </div>
   );
 }
