b287b7d81b85fa67f290101565598e967698ad89
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-09 12:33:22 -0500

    revert handleChange and handleClick

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import { useState } from "react";
 
 function CreateNote() {
+  const [input, setInput] = useState({
+    title: "",
+    content: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInput((prevInput) => {
+      return {
+        ...prevInput,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(input);
+  };
   return (
     <div className="container">
       <h3>Create a Note</h3>
-<<<<<<< HEAD
-=======
       <form>
         <div className="form-group mb-3">
           <input
+            value={input.title}
             name="title"
             autoComplete="off"
             type="text"
             className="form-control"
             placeholder="note title"
+            onChange={handleChange}
           />
         </div>
         <div>
@@ -21,14 +40,21 @@ function CreateNote() {
             placeholder="note content"
             className="form-control mb-3"
             autoComplete="off"
+            onChange={handleChange}
+            name="content"
+            value={input.content}
           />
         </div>
 
-        <button type="button" name="content" className="btn btn-success">
+        <button
+          type="button"
+          name="content"
+          className="btn btn-success"
+          onClick={handleClick}
+        >
           add note
         </button>
       </form>
->>>>>>> parent of b4882f1 (FrontEnd: setup handleChange and handleClick)
     </div>
   );
 }
