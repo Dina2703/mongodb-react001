c805960176ae25c060b5471068beafd79110d21f
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-09 13:18:53 -0500

    setting Notes page to display data fetched from backend

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 function Notes() {
+  const [notes, setNotes] = useState([
+    {
+      title: "",
+      content: "",
+    },
+  ]);
+
+  useEffect(() => {
+    fetch("/notes")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((data) => setNotes(data));
+  });
   return (
     <div className="container">
       <h3>Notes</h3>
+      {notes.map((note) => (
+        <div>
+          <h1>{note.title}</h1>
+          <p>{note.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
